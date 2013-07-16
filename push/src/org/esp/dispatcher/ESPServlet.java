7abcf95396b4717e3dc3ef25ab089a069395b9f9
arvinCao <cjwben0627@163.com>
2013-07-16 14:40:19 +0800

    第一次提交

---
+++ push/src/org/esp/dispatcher/ESPServlet.java
@@ -0,0 +1,21 @@
+package org.esp.dispatcher;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * 
+ * @author Arvin
+ *
+ */
+public class ESPServlet extends HttpServlet{
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		
+	}
+	
+	
+}
